c10040675ae1b33d550ec663cf8340e4b84f341c
veerkumar patil <veepatil@redhat.com>
2023-12-18 12:45:19 +0530

    First commit message

---
+++ GenerateTestCase.js
@@ -0,0 +1,57 @@
+const {VertexAI} = require('@google-cloud/vertexai');
+const axios = require('axios'); 
+
+async function generateContent() {
+    try {
+        let id=process.argv[2];
+        //Get the description of the Jira ticket 
+        const response = await axios.get("https://veer1109.atlassian.net/rest/api/2/issue/"+id+"?fields=description",{
+            headers: {
+                "Authorization": `Basic dmVlci4xMTA5QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBTRmtWeWR6b3VJaDBmcmVBamJ0d1lIc3lvaDE5dmxBeFhibXllOF9jSmZNTkFmOXNWbnB0dDdxZVA3eXpuSjlvNjYySjlkZURUNGhEZTBES1VQY093VVk0cGlEdUNycXpOdGxxR0tSZzZGM2Y3WjE3dDZrdndGYjV4Z0lhdVdiTWw5R0RYN293MWZmclRDZXV5QnhwSzgzV0dpQ1FhQXNhNVppekhuYjJYWEk9NkNGRDlBODg=`
+            } 
+        });
+        let desc = response.data.fields.description
+
+        const vertex_ai = new VertexAI({project: 'sage-tribute-408104', location: 'us-central1'});
+        const model = 'gemini-pro';
+        
+        
+        // Instantiate the models
+        const generativeModel = vertex_ai.preview.getGenerativeModel({
+          model: model,
+          generation_config: {
+            "max_output_tokens": 5000,
+            "temperature": 0.1,
+            "top_p": 1
+        },
+        });
+      const req = {
+        contents: [{role: 'user', parts: [{text: 'Give me Test cases in cypress from ' + desc }]}],
+      };
+    
+      const streamingResp = await generativeModel.generateContent(req);
+     // console.log(streamingResp.response.candidates.map(item => item.content.parts[0].text));
+      let body = streamingResp.response.candidates.map(item => item.content.parts[0].text);
+      body = body.toString().replace(/[\[\]']+/g, '');
+      console.log(body);
+    //   const dataToSend = {
+    //     data
+    // };
+    // const postres = await axios.post("https://veer1109.atlassian.net/rest/api/2/issue/GEN-1/comment", {
+    //     body: body
+    // },{
+    //     headers: {
+    //         headers: {
+    //             "Authorization": `Basic dmVlci4xMTA5QGdtYWlsLmNvbTpBVEFUVDN4RmZHRjBTRmtWeWR6b3VJaDBmcmVBamJ0d1lIc3lvaDE5dmxBeFhibXllOF9jSmZNTkFmOXNWbnB0dDdxZVA3eXpuSjlvNjYySjlkZURUNGhEZTBES1VQY093VVk0cGlEdUNycXpOdGxxR0tSZzZGM2Y3WjE3dDZrdndGYjV4Z0lhdVdiTWw5R0RYN293MWZmclRDZXV5QnhwSzgzV0dpQ1FhQXNhNVppekhuYjJYWEk9NkNGRDlBODg=`
+    //         } 
+    //     }
+    // });
+    }
+    catch(error) {
+        console.error("Error fetching data:", error);
+    }
+    // Initialize Vertex with your Cloud project and location
+    
+};
+
+generateContent();
